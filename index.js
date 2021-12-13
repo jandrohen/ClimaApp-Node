adfddae952467c39993dba660f15964f87804eea
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-12-13 19:51:26 +0100

    (lesson 69 course node) Add model to control the app

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt;
+
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+
+    switch (opt) {
+      case 1:
+        //Mostrar mensaje
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+
+        //Buscar los lugares
+
+        //Seleccionar el lugar
+
+        //Clima
+
+        //Mostrar resultados
+        console.log("\nInformacion de la ciudad\n".green);
+        console.log("Ciudad: ");
+        console.log("Lat: ");
+        console.log("Lng: ");
+        console.log("Temperatura: ");
+        console.log("Mínima: ");
+        console.log("Máxima: ");
+        break;
+    }
 
     if (opt !== 0) await pausa();
   } while (opt !== 0);
---
+++ models/busquedas.js
@@ -0,0 +1,18 @@
+
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor() {
+        //HACER: leer DB si existe
+    }
+
+    async ciudad (lugar = ''){
+
+        // petición http
+        console.log(lugar);
+
+        return []; // retorna los lugares
+    }
+}
+
+module.exports = Busquedas;
